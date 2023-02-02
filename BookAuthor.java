62b6f8b1fc1e74d4f85ce4544c80d2baeba6c2c5
Sannvg <117364049+Sannvg@users.noreply.github.com>
2023-02-02 17:39:37 +0400

    Aggreggation

---
+++ BookAuthor.java
@@ -0,0 +1,23 @@
+package com.obsqura.training.aggreggation;
+
+public class BookAuthor {
+	String bknm;
+	float bkprice;
+	Author bkauthor;
+	public BookAuthor(String bknm, float bkprice, Author bkauthor) {
+		super();
+		this.bknm = bknm;
+		this.bkprice = bkprice;
+		this.bkauthor = bkauthor;
+	}
+	void bookDetails() {
+		System.out.println("Book Details");
+		System.out.println("Book Name: "+bknm+'\n'+"Author: "+bkauthor.authnm+'\n'+"Book Price: "+bkprice);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Author auth1 = new Author("Paulo Coelho","Brazil", 55);
+		BookAuthor bk1 = new BookAuthor("The Alchemist",300f,auth1);
+		bk1.bookDetails();
+	}
+}
