9cc47705ea20c1bda32f16250f9fcb173f2ee9b4
Sannvg <117364049+Sannvg@users.noreply.github.com>
2023-02-02 17:41:02 +0400

    Exceptions

---
+++ ThrowException.java
@@ -0,0 +1,17 @@
+package com.obsqura.training.exceptions;
+
+public class ThrowException {
+
+	public static void main(String[] args) {
+		/*try {
+		if(10/0 ==1)
+			throw new ArithmeticException();
+		}catch(ArithmeticException e)
+		{
+			System.out.println("Not Divisibile");
+		}*/
+		if(10/0 ==1)
+			throw new ArithmeticException();
+	}
+
+}
